30e83df77ec849eae1b7283eaf47ad5f50625bbe
TheCmar7 <runner.cram@gmail.com>
2019-02-19 13:42:54 -0800

    added some style to make it nicer and fixed some major bugs that I introduced with the selection feature

--- mastermind.js
+++ mastermind.js
@@ -3,8 +3,8 @@ var game = (function() {
     var maxItems = 8;
     var pattern = [];
     var guesses = [];
-    var currentGuess = [0, 0, 0, 0, ];
-    var selected = 1;
+    var currentGuess = [0, 0, 0, 0];
+    var selected = 0;
 
     var colors = {
         0: 'brown',
@@ -58,6 +58,7 @@ var game = (function() {
      * and sets all the 0s to dashes
      */
     function updateDisplay() {
+        console.log(currentGuess);
         var columns = document.getElementsByClassName('column');
         for (var i = 0; i < columns.length; i++) {
             cell = columns[i];
@@ -67,44 +68,67 @@ var game = (function() {
             color = colors[parseInt(cell.innerHTML)];
             if (cell.id == ('guessed' + selected)) {
                 cell.style = "color: " + colors[parseInt(cell.innerHTML)] + "; background-color: " +
-                    colors[parseInt(cell.innerHTML)] + "; border: lightgray solid 2px;"
+                    colors[parseInt(cell.innerHTML)] + "; border: lightgray solid 3px;"
             } else {
                 cell.style = "color: " + colors[parseInt(cell.innerHTML)] + "; background-color: " + colors[parseInt(cell.innerHTML)] + ";"
             }
         }
     }
 
-    function highlightSelection(rowNum, clicked) {
-        var columns = document.querySelectorAll(".guessed");
-        for (var i = 0; i < columns.length; i++) {
-            columns[i].style = "font-weight:none;";
-        }
-        clicked.style = ""
-        updateDisplay();
-    }
-
     /**
      *  updateGuessedDisplay
      *
-     * 
+     * changed the 4 guess buttons to  the current guess array
      */
     function updateGuessedDisplay() {
-        $('#guessed1').html(currentGuess[0]);
-        $('#guessed2').html(currentGuess[1]);
-        $('#guessed3').html(currentGuess[2]);
-        $('#guessed4').html(currentGuess[3]);
+        $('#guessed0').html(currentGuess[0]);
+        $('#guessed1').html(currentGuess[1]);
+        $('#guessed2').html(currentGuess[2]);
+        $('#guessed3').html(currentGuess[3]);
         updateDisplay();
     }
 
-    function guessPart(guessedNum) {
+    function moveSelected() {
+        var moveBy = 1;
+        if (!currentGuess.includes(0)) {
+            moveBy = 0;
+        } else {
+            if (selected != 3 && currentGuess[selected + 1] == 0) {
+                moveBy = 1;
+            }
+            if (selected == 3 && currentGuess[selected - 1] == 0) {
+                moveBy = -1;
+            }
+        }
+        if (currentGuess[selected + moveBy] != 0) {
+            moveBy = 0;
+        }
+        setSelected(selected + moveBy);
+    }
 
+    /**
+     * guessPart
+     *
+     * @param {*} guessedNum
+     */
+    function guessPart(guessedNum) {
         if (!currentGuess.includes(guessedNum)) {
-            currentGuess[selected - 1] = guessedNum;
+            currentGuess[selected] = guessedNum;
+            moveSelected();
         }
         updateGuessedDisplay();
         updateDisplay();
     }
 
+
+    /**
+     * guessResult
+     *
+     * returns an array of 1s and 2s according the current guess as compared to 
+     * to the pattern
+     * 
+     * @returns {Array}
+     */
     function guessResult() {
         result = [];
         for (var i = 0; i < pattern.length; i++) {
@@ -123,29 +147,31 @@ var game = (function() {
      * compares the current guess to the chosen pattern and sets up the 
      */
     function submitGuess() {
-        if (currentGuess.length == 4) {
+        if (!currentGuess.includes(0)) {
             guesses.push(currentGuess);
+
+            $('#row' + guesses.length).find('.col1').html(currentGuess[0]);
+            $('#row' + guesses.length).find('.col2').html(currentGuess[1]);
+            $('#row' + guesses.length).find('.col3').html(currentGuess[2]);
+            $('#row' + guesses.length).find('.col4').html(currentGuess[3]);
+
+            result = guessResult();
+
+            if (result[0] == 2 && result[1] == 2 && result[2] == 2 && result[3] == 2) {
+                $('#result').html("YOU WIN");
+            } else {
+                $('#row' + guesses.length).find('.result').html(result.toString());
+            }
+
+            currentGuess = [0, 0, 0, 0];
+            updateGuessedDisplay();
+            setSelected(0);
+            updateDisplay();
+            if (guesses.length >= 12) {
+                $('#result').html("YOU LOSE!");
+            }
+
         }
-        $('#row' + guesses.length).find('.col1').html(currentGuess[0]);
-        $('#row' + guesses.length).find('.col2').html(currentGuess[1]);
-        $('#row' + guesses.length).find('.col3').html(currentGuess[2]);
-        $('#row' + guesses.length).find('.col4').html(currentGuess[3]);
-
-        result = guessResult();
-
-        if (result[0] == 2 && result[1] == 2 && result[2] == 2 && result[3] == 2) {
-            $('#result').html("YOU WIN");
-        } else {
-            $('#row' + guesses.length).find('.result').html(result.toString());
-        }
-
-        currentGuess = [0, 0, 0, 0];
-        updateGuessedDisplay();
-        updateDisplay();
-        if (guesses.length >= 12) {
-            $('#result').html("YOU LOSE!");
-        }
-
     }
 
 
@@ -160,6 +186,11 @@ var game = (function() {
         updateDisplay();
     }
 
+    /**
+     * setupButtons
+     *
+     * Sets all the button inputs
+     */
     function setupButtons() {
         $('.guessBtn').click(function() {
             guessPart(parseInt(this.getAttribute("data-number")))
@@ -170,11 +201,14 @@ var game = (function() {
         $('.guessed').click(function() {
             var selectedGuessedBtnNum = parseInt(this.getAttribute("data-number"));
             selectGuessSection(selectedGuessedBtnNum, this);
-            // highlightSelection(selectedGuessedBtnNum, this);
-
         })
     }
 
+    /**
+     * startGame
+     *
+     * starts the game
+     */
     function startGame() {
         updateDisplay();
         choosePattern();
@@ -182,10 +216,25 @@ var game = (function() {
 
     }
 
+    /**
+     * getPattern
+     *
+     * returns the pattern of the game
+     * 
+     * @returns {Array} pattern
+     */
     function getPattern() {
         return pattern;
     }
 
+
+    /**
+     * setPattern
+     * 
+     * allows for an array of 4 to be set as the pattern of the game
+     * 
+     * @param {Array} inputPattern
+     */
     function setPattern(inputPattern) {
         if (inputPattern.length == 4) {
             pattern = inputPattern;
